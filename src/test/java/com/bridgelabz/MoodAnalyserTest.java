ce292e9dd7631e6c32d8e89e39196b15917c7668
kirtibhave <kirtibhave1000@gmail.com>
2020-02-21 11:26:38 +0530

    ADDED:Use reflaction to create moodAnalyser with default constructor

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,23 +1,104 @@
 package com.bridgelabz;
-
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_WhenProper_ShouldReturnTrue() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy mood");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("Happy",mood);
+    public void givenNullMessage_ShouldReturnHappy() throws AnalyseMoodException {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+            String mood = MoodAnalyser.analyseMood();
+            Assert.assertEquals("Happy", mood);
+        } catch (AnalyseMoodException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void givenMessage_WhenNotProper_ShouldReturnFalse() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("Sad",mood);
+    public void givenNullMessage_ShouldReturnSad() throws AnalyseMoodException {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Sad", mood);
+        } catch (AnalyseMoodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenNullAnyMood_ShouldReturnHappy() throws AnalyseMoodException {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy", mood);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void givenEmptyString() {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("");
+            String mood = MoodAnalyser.analyseMood();
+            Assert.assertEquals("", mood);
+        } catch (AnalyseMoodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenMoodAnalyser_WhenProper_ShouldReturnObject() {
+        try {
+            Constructor<?> constructor = Class.forName("com.MoodAnalyser").getConstructor(String.class);
+            Object object = constructor.newInstance("I am in happy mood");
+            MoodAnalyser moodAnalyser = (MoodAnalyser) object;
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy", mood);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (AnalyseMoodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void okjoij() {
+        try {
+            MoodAnalyser moodAnalyser = MoodAnalyzeFactory.createMoodAnalyzer("I am in happy mood");
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy",mood);
+        }
+        catch (AnalyseMoodException e){
+            e.printStackTrace();
+
+
+    }
+
+    @Test
+    public void givenMoodAnalyzerClass_WhenProper_ShouldReturnObject() {
+        try {
+            MoodAnalyser moodAnalyser = MoodAnalyzeFactory.createMoodAnalyzer("I am in happy mood");
+           // String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy", mood);
+        } catch (AnalyseMoodException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
 
 
 
